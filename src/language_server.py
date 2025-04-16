8e0f9e8ce799f37f12224dad06638575d3213693
hyhping2023 <2632595396@qq.com>
2025-04-16 20:28:15 +0800

    add image query

--- src/language_server.py
+++ src/language_server.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import base64
 
 EMOTION_PROMPT = "You are a helpful desktop pet. You should try your best to serve me. Your owner is {}. You should use your word to share your owner's feelings" 
 CHAT_HISTORY = "tmp/chat_history.jsonl"
@@ -30,6 +31,18 @@ def generate(prompt):
         f.write(json.dumps({"role": "user", "content": prompt}) + "\n")
         f.write(json.dumps({"role": "assistant", "content": response.json()['message']['content']}) + "\n")
     return response.json()['message']['content']
+
+def scene_analyze():
+    image_base64 = tmp_picture_encode()
+    response = requests.post("http://localhost:11434/api/scene_analyze", json={
+        
+    })
+
+def tmp_picture_encode():
+    with open("tmp/capture/captrue_0.png", "rb") as f:
+        image_data = f.read()
+    image_base64 = base64.b64encode(image_data).decode('utf-8')
+    return image_base64
 
 if __name__ == "__main__":
     prompt = "请问神里绫华是谁"
