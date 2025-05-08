872766760db905b432c990f30eee6d35538d44f5
hyhping2023 <yhuang704@connect.hkust-gz.edu.cn>
2025-05-08 08:57:12 +0800

    add refer voice sample

--- src/config.py
+++ src/config.py
@@ -2,12 +2,12 @@ AVAIABLE_VOICES = {"rencai": {
                         "language": "日文",
                         "sovit-weight": "SoVITS_weights_v3/井芹仁菜V3_e100_s1800.pth",
                         "gpt-weight": "GPT_weights_v3/井芹仁菜V3-e15.ckpt",
-                        "ref-wav": '/Users/hyhping/Music/people/rencai.wav'
+                        "ref-wav": 'asset/voice/ref/rencai.wav'
                         },
                    "486":{
                         "language": "中文",
                         "sovit-weight": "SoVITS_weights_v2/安和昴V2.5_e100_s2300.pth",
                         "gpt-weight": "GPT_weights_v2/安和昴V2.5-e30.ckpt",
-                        "ref-wav": '/Users/hyhping/Music/people/486.wav'
+                        "ref-wav": 'asset/voice/ref/486.wav'
                           },
                    }
