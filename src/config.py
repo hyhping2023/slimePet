03172acf2a7c5e6d560742708fe2440d9d43ab11
hyhping2023 <2632595396@qq.com>
2025-05-08 14:40:24 +0800

    add anno voice

--- src/config.py
+++ src/config.py
@@ -5,14 +5,21 @@ AVAIABLE_VOICES = {"rencai": {
                         "ref-wav": 'asset/voice/ref/rencai.wav'
                         },
                    "486":{
-                        "language": "中文",
+                        "language": "日文",
                         "sovit-weight": "SoVITS_weights_v2/安和昴V2.5_e100_s2300.pth",
                         "gpt-weight": "GPT_weights_v2/安和昴V2.5-e30.ckpt",
                         "ref-wav": 'asset/voice/ref/486.wav'
                           },
+                    "anno":{
+                        "language": "日文",
+                        "sovit-weight": "SoVITS_weights_v2/千早爱音V2_e20_s460.pth",
+                        "gpt-weight": "GPT_weights_v2/千早爱音V2-e30.ckpt",
+                        "ref-wav": 'asset/voice/ref/anno.wav'
+                        }
                    }
 
 NAME2REALNAME = {
     "rencai": "井芹仁菜",
     "486": "安和昴",
+    "anno": "千早爱音"
 }
